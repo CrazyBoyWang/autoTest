eda32eafe85d25a074c0b1f41bca37eae5adbe34
wangxuesong <wangxuesong@xdf.cn>
2020-09-27 15:54:00 +0800

    1、收货地址整合代码调整

--- src/main/java/com/xdf/bling/qa/ios/service/mine/CouponService.java
+++ src/main/java/com/xdf/bling/qa/ios/service/mine/CouponService.java
@@ -25,16 +25,15 @@ public class CouponService {
         couponPage.clickMineTab();
         Thread.sleep(2000);
         couponPage.clickCouponPage();
-        if (couponPage.isCouponPage().equals("兑换")) {
-            Thread.sleep(3000);
-            couponPage.goBack();
-        }
+        Thread.sleep(2000);
+        couponPage.goBack();
+    }
 //        Assert.assertTrue(couponPage.isShopPage(), "//进入优惠券失败");
 //        try {
 //            Thread.sleep(4000);
 //        } catch (InterruptedException e) {
 //            e.printStackTrace();
 //        }
-        // myOrderPage.clickAllOrderList();
-    }
+    // myOrderPage.clickAllOrderList();
+
 }
